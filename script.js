788c633a93e223f08321a463d4cb1022c4285cfc
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-01-09 19:53:53 -0800

    fix popper width and remove dead code

--- script.js
+++ script.js
@@ -1,130 +1,130 @@
 var recipeSet = [];
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
-// input    
-// var input = document.querySelector('#searchBar').value
-// var queryURL = "https://api.edamam.com/search?q=" + input + "&from=0&to=100&app_id=de5d421e&app_key=3b067d8684260b2f7abcb8eb43481d4c" 
-// health labels
-var healthLabel = "&health=";
-// diets
-var keto = "keto-friendly";
-var pesca = "pescatarian";
+    // input    
+    // var input = document.querySelector('#searchBar').value
+    // var queryURL = "https://api.edamam.com/search?q=" + input + "&from=0&to=100&app_id=de5d421e&app_key=3b067d8684260b2f7abcb8eb43481d4c" 
+    // health labels
+    var healthLabel = "&health=";
+    // diets
+    var keto = "keto-friendly";
+    var pesca = "pescatarian";
 
-// allowable filters
-var balanced = "balanced";
-var highProtein = "high-protein"
-var lowFat = "low-fat";
-var lowCarb = "low-carb";
-var sugarConscious = "sugar-conscious";
-var peanutFree = "peanut-free";
-var treeNutFree = "tree-nut-free";
-var alcoholFree = "alcohol-free";
-var vegan = "vegan";
-var vegetarian = "vegetarian";
-
-
-// intolerances
-var gluten = "gluten-free";
-var lactoseIntolerant = "dairy-free";
-// meal type
-var mealType = "&mealType=";
-var breakfast = "Breakfast";
-var lunch = "Lunch";
-var dinner = "Dinner";
-// cuisine type
-var cuisineType = "&cuisineType=";
-var american = "American";
-var asian = "Asian";
-var british = "British";
-var caribbean = "Caribbean";
-var centralEurope = "Central Europe";
-var chinese = "Chinese";
-var easternEurope = "Eastern Europe";
-var french = "French";
-var indian = "Indian";
-var italian = "Italian";
-var japanese = "Japanese";
-var kosher = "Kosher";
-var mediterranean = "Mediterranean";
-var mexican = "Mexican";
-var middleEastern = "Middle Eastern";
-var nordic = "Nordic";
-var southAmerican = "South American";
-var southEastAsian = "South East Asian";
+    // allowable filters
+    var balanced = "balanced";
+    var highProtein = "high-protein"
+    var lowFat = "low-fat";
+    var lowCarb = "low-carb";
+    var sugarConscious = "sugar-conscious";
+    var peanutFree = "peanut-free";
+    var treeNutFree = "tree-nut-free";
+    var alcoholFree = "alcohol-free";
+    var vegan = "vegan";
+    var vegetarian = "vegetarian";
 
 
-
-
-var setnumber = 0;
-
-// getRecipe();
+    // intolerances
+    var gluten = "gluten-free";
+    var lactoseIntolerant = "dairy-free";
+    // meal type
+    var mealType = "&mealType=";
+    var breakfast = "Breakfast";
+    var lunch = "Lunch";
+    var dinner = "Dinner";
+    // cuisine type
+    var cuisineType = "&cuisineType=";
+    var american = "American";
+    var asian = "Asian";
+    var british = "British";
+    var caribbean = "Caribbean";
+    var centralEurope = "Central Europe";
+    var chinese = "Chinese";
+    var easternEurope = "Eastern Europe";
+    var french = "French";
+    var indian = "Indian";
+    var italian = "Italian";
+    var japanese = "Japanese";
+    var kosher = "Kosher";
+    var mediterranean = "Mediterranean";
+    var mexican = "Mexican";
+    var middleEastern = "Middle Eastern";
+    var nordic = "Nordic";
+    var southAmerican = "South American";
+    var southEastAsian = "South East Asian";
 
 
 
 
-$("#nextSet").on("click", function (event) {
-    
+    var setnumber = 0;
 
-    if(setnumber > 8) {
-        return;
-    }
-
-    else {
-    event.preventDefault();
-    setnumber++
-    renderList();
-    }
-})
+    // getRecipe();
 
 
-$("#previousSet").on("click", function (event) {
-    if(setnumber <= 0) {
-        return
-    }
 
-    else {
-        
-        event.preventDefault();
-        setnumber--
-        renderList();
-    }
-    
-})
 
-$("#searchButton").on("click", function(event) {
-    event.preventDefault();
-    
-    
-    
+    $("#nextSet").on("click", function (event) {
 
-    // var input = encodeURI(input)
-    
-    
-    // if (input === "") {
-    
-    // return;
-    
-    //  }
-    
-     
-    
-    getRecipe();
-    
-    
+
+        if (setnumber > 8) {
+            return;
+        }
+
+        else {
+            event.preventDefault();
+            setnumber++
+            renderList();
+        }
     })
-    
-function getRecipe() {
-    
-    var input = document.querySelector('#searchBar').value
-    var queryURL = "https://api.edamam.com/search?q=" + input + "&from=0&to=100&app_id=de5d421e&app_key=3b067d8684260b2f7abcb8eb43481d4c"
-    
+
+
+    $("#previousSet").on("click", function (event) {
+        if (setnumber <= 0) {
+            return
+        }
+
+        else {
+
+            event.preventDefault();
+            setnumber--
+            renderList();
+        }
+
+    })
+
+    $("#searchButton").on("click", function (event) {
+        event.preventDefault();
+
+
+
+
+        // var input = encodeURI(input)
+
+
+        // if (input === "") {
+
+        // return;
+
+        //  }
+
+
+
+        getRecipe();
+
+
+    })
+
+    function getRecipe() {
+
+        var input = document.querySelector('#searchBar').value
+        var queryURL = "https://api.edamam.com/search?q=" + input + "&from=0&to=100&app_id=de5d421e&app_key=3b067d8684260b2f7abcb8eb43481d4c"
+
 
 
         if (document.getElementById("vegan").checked === true) {
-            
+
             var queryURL = queryURL.concat(healthLabel, vegan)
 
         }
@@ -144,22 +144,22 @@ function getRecipe() {
         //     var queryURL = queryURL.concat(healthLabel, lowCarb)
         // }
 
-        if ( document.getElementById("sugar-conscious").checked === true) {
+        if (document.getElementById("sugar-conscious").checked === true) {
 
             var queryURL = queryURL.concat(healthLabel, sugarConscious)
         }
 
-        if ( document.getElementById("peanut-free").checked === true) {
+        if (document.getElementById("peanut-free").checked === true) {
 
             var queryURL = queryURL.concat(healthLabel, peanutFree)
         }
 
-        if ( document.getElementById("tree-nut-free").checked === true) {
+        if (document.getElementById("tree-nut-free").checked === true) {
 
             var queryURL = queryURL.concat(healthLabel, treeNutFree)
         }
 
-        if ( document.getElementById("vegetarian").checked === true) {
+        if (document.getElementById("vegetarian").checked === true) {
 
             var queryURL = queryURL.concat(healthLabel, vegetarian)
         }
@@ -170,127 +170,127 @@ function getRecipe() {
         // }
 
         if (document.getElementById("alcohol-free").checked === true) {
-            
+
             var queryURL = queryURL.concat(healthLabel, alcoholFree)
 
         }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
 
 
-    }).then(function(response) {
-        
-        
-        console.log(queryURL);
-       
-        setnumber = 0;
-        recipeSet = response.hits
-        
-        renderList();
-        
-    
+        }).then(function (response) {
 
-        
-// user click on next set
+
+            console.log(queryURL);
+
+            setnumber = 0;
+            recipeSet = response.hits
+
+            renderList();
 
 
 
 
-    })
-    
-}
-
-
-function renderList () {
+            // user click on next set
 
 
 
-    $("#accordion").text("")
 
-    for(var i = 0; i <= 9; i++){
-        
-        // recipe labels 
-        recipeSet[ setnumber*10+i ];
-            
-        li = $("<li>")
-        li.addClass("uk-margin-top")
-        
-        addRecipeButton = $("<button>" + "+" + "</button>")
-        addRecipeButton.addClass("uk-margin-left addRecipe")
-        
-        
-        recipeLabel = $("<a>" + recipeSet[ setnumber*10+i ].recipe.label + "</a>")
-        recipeLabel.append(addRecipeButton)
-        
-        
-
-
-        recipeLabel.attr("href", "#")
-        recipeLabel.attr("data-recipe-id", recipeSet[ setnumber*10+i ].recipe.uri)
-        // recipeLabel.attr("data", `label-${i}`)
-        // recipeLabel.data("recipeId",recipeSet[ setnumber*10+i ].recipe.uri)
-        recipeLabel.addClass("uk-accordion-title uk-margin-top")
-        
-        
-        
-        // accordion div
-        accordionDiv = $("<div>");
-        accordionDiv.addClass("uk-accordion-content")
-        // accordionDiv.attr("data", `details-${i}`)
-
-        // ingredients header ul
-
-        ingredientsUl = $("<ul>" + 'Ingredients' + "</ul>");
-        ingredientsUl.addClass("")
-
-        
-
-        // recipe ingredients
-
-        
-
-        
-        $.each(recipeSet[ setnumber*10+i ].recipe.ingredientLines, function (index, value) {
-            
-            
-
-            recipeIngredients = $("<li>" + value + "</li>")
-            recipeIngredients.addClass("uk-padding-small")
-    
-            ingredientsUl.append(recipeIngredients)
-
-            
         })
 
+    }
 
-        // recipe image
 
-        recipeImg = $("<img>").css({ 'height': '150px', 'width': '200px', 'float': 'right'});
-            
-            recipeImg.attr("src", recipeSet[ setnumber*10+i ].recipe.image);
+    function renderList() {
+
+
+
+        $("#accordion").text("")
+
+        for (var i = 0; i <= 9; i++) {
+
+            // recipe labels 
+            recipeSet[setnumber * 10 + i];
+
+            li = $("<li>")
+            li.addClass("uk-margin-top")
+
+            addRecipeButton = $("<button>" + "+" + "</button>")
+            addRecipeButton.addClass("uk-margin-left addRecipe")
+
+
+            recipeLabel = $("<a>" + recipeSet[setnumber * 10 + i].recipe.label + "</a>")
+            recipeLabel.append(addRecipeButton)
+
+
+
+
+            recipeLabel.attr("href", "#")
+            recipeLabel.attr("data-recipe-id", recipeSet[setnumber * 10 + i].recipe.uri)
+            // recipeLabel.attr("data", `label-${i}`)
+            // recipeLabel.data("recipeId",recipeSet[ setnumber*10+i ].recipe.uri)
+            recipeLabel.addClass("uk-accordion-title uk-margin-top")
+
+
+
+            // accordion div
+            accordionDiv = $("<div>");
+            accordionDiv.addClass("uk-accordion-content")
+            // accordionDiv.attr("data", `details-${i}`)
+
+            // ingredients header ul
+
+            ingredientsUl = $("<ul>" + 'Ingredients' + "</ul>");
+            ingredientsUl.addClass("")
+
+
+
+            // recipe ingredients
+
+
+
+
+            $.each(recipeSet[setnumber * 10 + i].recipe.ingredientLines, function (index, value) {
+
+
+
+                recipeIngredients = $("<li>" + value + "</li>")
+                recipeIngredients.addClass("uk-padding-small")
+
+                ingredientsUl.append(recipeIngredients)
+
+
+            })
+
+
+            // recipe image
+
+            recipeImg = $("<img>").css({ 'height': '150px', 'width': '200px', 'float': 'right' });
+
+            recipeImg.attr("src", recipeSet[setnumber * 10 + i].recipe.image);
 
             recipeImg.addClass("uk-text-center")
-            
-           
 
-        // recipe url
+
+
+            // recipe url
 
             recipeUrl = $("<div>");
-            recipeUrlLink = $("<a>" + recipeSet[ setnumber*10+i ].recipe.url + "</a>");
+            recipeUrlLink = $("<a>" + recipeSet[setnumber * 10 + i].recipe.url + "</a>");
             recipeUrl.append(recipeUrlLink);
-            
-        
+
+
 
             accordionDiv.append(recipeImg, ingredientsUl, recipeUrl)
             li.append(recipeLabel, accordionDiv)
             $("#accordion").append(li)
-            
-    }
-    
-   
 
-}
+        }
+
+
+
+    }
 
 });
